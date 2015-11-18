10e2204b01d325c8a800689c55d13d474d8c3af5
antkahn <ant.kahn@gmail.com>
2015-11-18 14:26:37 +0100

    edit question

--- src/AppBundle/Controller/QuestionController.php
+++ src/AppBundle/Controller/QuestionController.php
@@ -35,4 +35,27 @@ class QuestionController extends Controller
 
         return $this->render(':question:add.html.twig', ['form' => $form->createView()]);
     }
+
+    /**
+     * @Route("/question/{id}/modifier", name="question_edit")
+     */
+    public function editAction(Request $request, Question $question)
+    {
+        $category = $question->getCategory();
+
+        $form = $this->createForm(new QuestionType(), $question);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted()) {
+
+            $em = $this->getDoctrine()->getEntityManager();
+
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('category_show', ['id' => $category->getId()]));
+        }
+
+        return $this->render(':question:edit.html.twig', ['form' => $form->createView()]);
+    }
 }
