949382fe4b9d8bc5494391959c0e0aaf814d18aa
antkahn <ant.kahn@gmail.com>
2016-05-10 23:29:41 +0200

    temporary delete random

--- src/AppBundle/Repository/QuestionRepository.php
+++ src/AppBundle/Repository/QuestionRepository.php
@@ -14,8 +14,6 @@ class QuestionRepository extends EntityRepository
     public function getRandomQuestions()
     {
         return  $this->createQueryBuilder('q')
-            ->addSelect('RAND() as HIDDEN rand')
-            ->addOrderBy('rand')
             ->setMaxResults(20)
             ->getQuery()
             ->getResult();
@@ -32,8 +30,6 @@ class QuestionRepository extends EntityRepository
         return  $this->createQueryBuilder('q')
             ->andWhere('q.category = :category')
             ->setParameter('category', $category)
-            ->addSelect('RAND() as HIDDEN rand')
-            ->addOrderBy('rand')
             ->setMaxResults($count)
             ->getQuery()
             ->getResult();
